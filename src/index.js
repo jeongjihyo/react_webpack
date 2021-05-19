5b1f1892c21939b2c5008bf2c4d78ddce8791f7f
jeongjihyo <glgjihyo@gmail.com>
2021-05-19 16:19:02 +0900

    webpack.config.js에 output 동적 생성할 수 있도록 변경, html-webpack-plugin 플러그인 추가
     - html-webpack-plugin : 웹팩으로 빌드한 결과물로 HTML 파일을 자동으로 생성해주는 플러그인
     - dist 폴더 정리 => output에 clean: true 추가

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from "lodash";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "webpack", "jihyo"], " ");
+
+  btn.innerHTML = "나를 클릭해봐!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 
+//웹팩으로 빌드한 결과물로 HTML 파일을 생성해주는 플러그인
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
