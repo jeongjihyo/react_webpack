cb3720a63a155bd9136e7e43828de1f46892e673
jeongjihyo <glgjihyo@gmail.com>
2021-05-20 22:17:02 +0900

    react에서 Typescript 사용하기 위해 ts => tsx 로 확장자 변경
     - 순수 Typescript 파일에는 .ts 를 사용, jsx가 포함 된 파일에는 .tsx 를 사용

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
